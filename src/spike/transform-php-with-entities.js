50ae6e60c545ab7d78c9bc3404fdd1cfaebfbe04
Adam Kiss <i.am.adam.kiss+git@gmail.com>
2018-09-03 12:16:19 +0200

    Add PHP entity recoding

---
+++ src/spike/transform-php-with-entities.js
@@ -0,0 +1,21 @@
+// Transform generated <php> tags to real PHP tags and rename files
+
+const tag = /<php>([\S\s]*?)<\/php>\n?/g
+const echo = /<php echo>([\S\s]*?)<\/php>/g
+const only = /<php only>([\S\s]*?)<\/php>/g
+
+function recodeEntities(content) {
+	return content
+		.replace('&amp;', '&')
+		.replace('&gt;', '>')
+		.replace('&lt;', '<')
+}
+
+module.exports = {
+	match: name => name.endsWith('.php.html'),
+	rename: name => name.replace('.php.html', '.php'),
+	replace: f => f
+		.replace(tag, (all, content) => `<?php ${recodeEntities(content)} ?>`)
+		.replace(echo, (all, content) => `<?= ${recodeEntities(content)} ?>`)
+		.replace(only, (all, content) => `<?php ${recodeEntities(content)}`)
+}
